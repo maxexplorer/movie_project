9e9077c2b9c1630bcfaed31c1c930c333225d7b0
maxexplorer <maxexplorer@mail.ru>
2022-04-05 08:34:38 +0300

    Создан роут, функции-представления и шаблоны для отображения информации о всех актерах и детальная информация о каждом актере.

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.show_all_movies),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie_detail'),
-    path('director', views.show_all_directors),
-    path('director/<slug:slug_director>', views.show_director, name='info_director'),
+    path('directors', views.show_all_directors),
+    path('actors', views.show_all_actors),
+    path('directors/<slug:slug_director>', views.show_director, name='info_director'),
+    path('actors/<slug:slug_actor>', views.show_actor, name='info_actor'),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Sum, Max, Min, Count, Avg
 
 # Create your views here.
-from .models import Movie, Director
+from .models import Movie, Director, Actor
 
 def show_all_movies(request):
     movies = Movie.objects.order_by('-rating')
@@ -26,8 +26,21 @@ def show_all_directors(request):
         'directors': directors
     })
 
+def show_all_actors(request):
+    actors = Actor.objects.order_by('last_name')
+    return render(request, 'movie_app/all_actors.html', {
+        'actors': actors
+    })
+
 def show_director(request, slug_director: str):
     director = get_object_or_404(Director, slug=slug_director)
     return render(request, 'movie_app/info_director.html', {
         'director': director
+    })
+
+
+def show_actor(request, slug_actor: str):
+    actor = get_object_or_404(Actor, slug=slug_actor)
+    return render(request, 'movie_app/info_actor.html', {
+        'actor': actor
     })
